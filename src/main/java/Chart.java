f1c6ad9008d3c995614c481bfc99c626926f8ebe
Eduardo Jandre <eduardojandre@gmail.com>
2018-09-06 00:37:19 -0300

    First Commit

---
+++ src/main/java/Chart.java
@@ -0,0 +1,54 @@
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.swing.JPanel;
+
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.XChartPanel;
+import org.knowm.xchart.XYChart;
+
+public class Chart {
+	final XYChart chart;
+	JPanel chartPanel;
+	String seriesName;
+	XChartPanel<XYChart> chartPane;
+	ArrayList<Double> tempData;
+	ArrayList<Double> timeData;
+	public Chart(String title,String eixoX,String eixoY,Double initialValue) {
+		
+	    tempData=new ArrayList<Double>();
+	    timeData=new ArrayList<Double>();
+
+	    
+	    timeData.add((double) 0);
+	    tempData.add((double) initialValue);
+		
+		chart = QuickChart.getChart(title, eixoX, eixoY, eixoY, timeData, tempData);
+		
+		seriesName=eixoY;
+
+	    chartPane = new XChartPanel<XYChart>(chart);
+	}
+	public XChartPanel<XYChart> getPanel(){
+		return chartPane;
+	}
+	public void updateData(Double newValue) {
+		Double last=timeData.get(timeData.size()-1);
+		if(timeData.size()>100) {
+			timeData.remove(0);
+			tempData.remove(0);
+		}
+		timeData.add(last+1);
+		tempData.add((double) newValue);
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+
+	          public void run() {
+
+	            chart.updateXYSeries(seriesName, timeData, tempData, null);
+	            chartPane.repaint();
+	          }
+	        });
+	}
+}
